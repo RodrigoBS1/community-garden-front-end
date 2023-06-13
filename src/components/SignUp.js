f23fa2cba68ea6c6f03eaf878ef319d105f55dc1
Rodrigo Silva <rodrigob_silva@hotmail.com>
2023-06-13 14:19:21 -0400

    Adjust css on SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -82,34 +82,34 @@ const SignUp = () => {
             <form onSubmit={handleSubmit} className="signupForm">
                 <div className="signForm">
                     <label htmlFor="firstName" >First Name: </label>
-                    <input onChange={handleChange} type="text" id="firstName" name="firstName" placeholder="First Name" />
+                    <input className='inputSignup' onChange={handleChange} type="text" id="firstName" name="firstName" placeholder="First Name" />
              
                     <label htmlFor="lastName" >Last Name: </label>
-                    <input onChange={handleChange} type="text" id="lastName" name="lastName" placeholder="Last Name" />
+                    <input className='inputSignup' onChange={handleChange} type="text" id="lastName" name="lastName" placeholder="Last Name" />
                 </div>
                 <div className="signForm">
                     <label htmlFor="userName" >Username: </label>
-                    <input onChange={handleChange} type="text" id="userName" name="userName" placeholder="Username" />
+                    <input className='inputSignup' onChange={handleChange} type="text" id="userName" name="userName" placeholder="Username" />
                 
                     <label htmlFor="password" >Password: </label>
-                    <input onChange={handleChange} type="text" id="password" name="password" placeholder="Password" />
+                    <input className='inputSignup' onChange={handleChange} type="text" id="password" name="password" placeholder="Password" />
                 </div>
                 <div className="signForm">
                     <label htmlFor="city" >City: </label>
-                    <input onChange={handleChange2} type="text" id="city" name="city" placeholder="City" />
+                    <input className='inputSignup' onChange={handleChange2} type="text" id="city" name="city" placeholder="City" />
              
                     <label htmlFor="state" >State: </label>
-                    <input onChange={handleChange} type="text" id="state" name="state" placeholder="State" />
+                    <input className='inputSignup' onChange={handleChange} type="text" id="state" name="state" placeholder="State" />
                 </div>
                 <div className="signForm">
                     <label htmlFor="email" >Email: </label>
-                    <input onChange={handleChange} type="text" id="email" name="email" placeholder="Email" />
+                    <input className='inputSignup' onChange={handleChange} type="text" id="email" name="email" placeholder="Email" />
              
                     <label htmlFor="phoneNumber" >Phone Number: </label>
-                    <input onChange={handleChange} type="text" id="phoneNumber" name="phoneNumber" placeholder="Phone Number" />
+                    <input className='inputSignup' onChange={handleChange} type="text" id="phoneNumber" name="phoneNumber" placeholder="Phone Number" />
                 </div>
-                <div className='button'>
-                <button>Find My Garden</button>
+                <div className='buttonSubmitGarden'>
+                <button className='buttonGarden'>Find My Garden</button>
                 </div>
             </form>
             <div className="localGardenDisplay" style={{display:showGardens}}>
